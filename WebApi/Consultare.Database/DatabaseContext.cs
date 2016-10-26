b132dcf337ca35280d0c077496f73a1860b0358b
Carol Braileanu <carol.braileanu@gmail.com>
2016-10-26 12:28:51 +0300

    First commit

---
+++ WebApi/Consultare.Database/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultare.Database
+{
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
+    {
+        public DatabaseContext(): base("Name=DatabaseContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+        }
+
+
+    }
+}
---
+++ WebApi/Consultare.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Consultare.WebApi.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("Register")]
+        public void RegisterUser(UserModel user)
+        {
+            
+        }
+    }
+}
