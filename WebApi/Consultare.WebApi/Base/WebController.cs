f3a15e5c861f972aa246622db28d82bfb7e5c806
Carol Braileanu <carol.braileanu@gmail.com>
2016-10-26 15:17:28 +0300

    Users and authorization done

---
+++ WebApi/Consultare.WebApi/Base/WebController.cs
@@ -0,0 +1,22 @@
+﻿using Consultare.Database;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Consultare.WebApi.Base
+{
+	public class WebController : ApiController
+    {
+        protected DatabaseContext context;
+        protected UserManager<IdentityUser> userManager;
+        public WebController()
+        {
+            this.context = new DatabaseContext();
+            this.userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(context));
+        }
+    }
+}
--- WebApi/Consultare.WebApi/Controllers/AccountController.cs
+++ WebApi/Consultare.WebApi/Controllers/AccountController.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using Consultare.Database;
+using Consultare.WebApi.Base;
+using Consultare.WebApi.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
 namespace Consultare.WebApi.Controllers
 {
-    [RoutePrefix("api/Account")]
-    public class AccountController : ApiController
+    [RoutePrefix("api/account")]
+    public class AccountController : WebController
     {
-        [AllowAnonymous]
-        [Route("Register")]
-        public void RegisterUser(UserModel user)
+        [Authorize]
+        [Route("register")]
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
         {
-            
+            IdentityUser user = new IdentityUser {
+                UserName = userModel.UserName,
+            };
+
+            var result = await this.userManager.CreateAsync(user, userModel.Password);
+            return result;
         }
     }
 }
